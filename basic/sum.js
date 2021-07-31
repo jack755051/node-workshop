8317d4374b1b623c83751df854057f0c2293f61e
jack755051 <jack755051@gmail.com>
2021-07-31 13:57:45 +0800

    修改

--- basic/sum.js
+++ basic/sum.js
@@ -1,13 +1,10 @@
 
-let result = 0;
-
 function sum(n){
+    let result = 0;
     for(var i=1;i<=n;i++){
             result+=i;
     }
-    // return result;
-
-    console.log(result);
+    return result;
 }
-
-sum(10)
+console.log(sum(10));
+console.log(sum(2));
