81d9fdb833eeb92013e9b93ab7302967b055814c
jack755051 <jack755051@gmail.com>
2021-07-31 14:19:29 +0800

    修改2次

--- basic/sum.js
+++ basic/sum.js
@@ -1,10 +1,19 @@
+function sum1(n){
+    return ((n+1)*n)/2;
+}
+
+
 
 function sum(n){
-    let result = 0;
+    let result2 = 0;
     for(var i=1;i<=n;i++){
             result+=i;
     }
     return result;
 }
-console.log(sum(10));
-console.log(sum(2));
+
+
+console.log(sum1(10),sum(10));
+console.log(sum1(3),sum(3));
+
+console
